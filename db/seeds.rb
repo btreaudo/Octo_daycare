94bfefe622839171a6acd66a6e1ad3907c93c272
Brenston Treaudo <btreaudo@gmail.com>
2015-08-18 09:04:39 -0500

    Added seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Breed.create(breed: "Mimic")
+Breed.create(breed: "Pygmy")
+Breed.create(breed: "Blue Ring")
+Breed.create(breed: "Caribbean Reef")
+Breed.create(breed: "Ribbon")
+Breed.create(breed: "North Pacific Giant")
+Breed.create(breed: "Seven Arm")
